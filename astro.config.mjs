77c43f1a6a61674b160ed3911c2dc04e66441b06
Miguel Benjamin Zubieta Rios <mbenjaminzr@gmail.com>
2024-04-15 10:48:51 -0400

    adding dependencies and initial configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,20 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
+
+import mdx from '@astrojs/mdx'
+import sitemap from '@astrojs/sitemap'
+import UnoCSS from 'unocss/astro'
+import vue from '@astrojs/vue'
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://hacklab-website.vercel.app/',
+  integrations: [
+    vue(),
+    mdx(),
+    UnoCSS(
+      { injectReset: true,
+      },
+    ),
+    sitemap(),
+  ],
+})
